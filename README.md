fbe2e00cd20ead4f3ef28ad431594f304991676e
Bibin <bibin3@gmail.com>
2020-06-22 15:03:30 +0530

    initial commits

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nodeserver
 node server functionalities 
+#npm install express multer sequelize pg pg-hstore --save
---
+++ app/config/env.js
@@ -0,0 +1,16 @@
+const env = {
+    database: 'test',
+    username: 'admin',
+    password: '123',
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+   
+  module.exports = env;
+  
